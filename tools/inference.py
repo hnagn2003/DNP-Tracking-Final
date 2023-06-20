b87d8cb76cd70b5a189a7eed127814206fad79f7
nhngan <hnagn2003@gmail.com>
2023-06-20 17:10:05 +0700

    saved

--- tools/inference.py
+++ tools/inference.py
@@ -6,8 +6,9 @@ from mmtrack.apis import inference_mot, init_model
 import os
 
 mot_config = './configs/mot/ocsort/ocsort_yolox_x_crowdhuman_mot17-private-half.py'
-input_folder = './data/DNP_test/video/'
-mot_model = init_model(mot_config, device='cuda:0')
+input_folder = './data/DNP/video/'
+checkpoint = './checkpoints/ocsort_yolox_x_crowdhuman_mot17-private-half_20220813_101618-fe150582.pth'
+mot_model = init_model(mot_config, checkpoint, device='cuda:0')
 out_dir = tempfile.TemporaryDirectory()
 out_path = out_dir.name
 pred_folder = "./output/pred"
@@ -23,19 +24,16 @@ for input_video in os.listdir(input_folder):
         for i, img in enumerate(imgs):
                 result = inference_mot(mot_model, img, frame_id=i)
                 out_data[i].append(result)
-                mot_model.show_result(
-                        img,
-                        result,
-                        show=False,
-                        wait_time=int(1000. / imgs.fps),
-                        out_file=f'{out_path}/{i:06d}.jpg')
+                # mot_model.show_result(
+                #         img,
+                #         result,
+                #         show=False,
+                #         wait_time=int(1000. / imgs.fps),
+                #         out_file=f'{out_path}/{i:06d}.jpg')
                 prog_bar.update()
         mmcv.dump(out_data, pred_file)
-        output = './demo/mot.mp4'
-        print(f'\n making the output video at {output} with a FPS of {imgs.fps}')
-        mmcv.frames2video(out_path, output, fps=imgs.fps, fourcc='mp4v')
-        out_dir.cleanup()
+        # output = './demo/mot.mp4'
+        # print(f'\n making the output video at {output} with a FPS of {imgs.fps}')
+        # mmcv.frames2video(out_path, output, fps=imgs.fps, fourcc='mp4v')
+        # out_dir.cleanup()
         print("--------------DONE! " + input_video)
-
-exit(0)
-# input_video = './data/DNP/video/output000.mp4'
